434bd5a6cb02167796aed938ec1d3bb065673f55
amcc <alistairmcclymont@gmail.com>
2023-05-30 14:27:02 +0100

    feat: better scroll with text

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const totalImages = 61;
 
-window.onload = function () {
+function calcDims(identity, container) {
+  let width = identity.offsetWidth;
+  container.style.height = width * 2 + "px";
+}
+
+window.onresize = function () {
+  const identity = document.getElementById("identity");
   const container = document.getElementById("identity-container");
+  calcDims(identity, container);
+};
+window.onload = function () {
+  const identity = document.getElementById("identity");
+  const container = document.getElementById("identity-container");
+
+  calcDims(identity, container);
+
   for (let i = 0; i < totalImages; i++) {
     let image = document.createElement("img");
     const number = String(i).padStart(2, "0");
@@ -9,35 +23,16 @@ window.onload = function () {
     image.setAttribute("src", "./jpegs/single_1" + number + ".jpg");
     image.setAttribute("alt", "RCA 2023 identity");
     i > 0 ? image.classList.add("hide") : image.classList.add("show");
-    container.appendChild(image);
+    identity.appendChild(image);
   }
 
-  const elements = [...container.children];
-
-  //   container.addEventListener("mousemove", function (event) {
-  //     let x = event.clientX;
-  //     let y = event.clientY;
-  //     let width = container.clientWidth;
-  //     let frame = Math.floor((x / width) * totalImages);
-  //     if (frame > totalImages - 1) frame = totalImages - 1;
-  //     // console.log(frame);
-
-  //     for (let i = 0; i < totalImages; i++) {
-  //       if (i === frame) {
-  //         elements[i].classList.add("show");
-  //         elements[i].classList.remove("hide");
-  //       } else {
-  //         elements[i].classList.add("hide");
-  //         elements[i].classList.remove("show");
-  //       }
-  //     }
-  //   });
+  const elements = [...identity.children];
 
   let lastKnownScrollPosition = 0;
   let ticking = false;
 
   function doSomething(scrollPos) {
-    let width = container.clientWidth;
+    let width = identity.offsetWidth;
     let frame = Math.floor((scrollPos / width) * totalImages);
 
     let seekFrame = frame > totalImages - 1 ? totalImages - 1 : frame;
@@ -55,9 +50,9 @@ window.onload = function () {
     if (scrollPos >= width) {
       let pos = scrollPos - width;
       console.log(pos);
-      container.style.top = -pos + "px";
+      identity.style.top = -pos + "px";
     } else {
-      container.style.top = "0px";
+      identity.style.top = "0px";
     }
   }
 
