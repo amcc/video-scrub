5fba040e3278b1e43904ceab6866ca0f37adce55
amcc <alistairmcclymont@gmail.com>
2023-05-30 13:59:29 +0100

    feat: scroll pos

--- script.js
+++ script.js
@@ -14,16 +14,37 @@ window.onload = function () {
 
   const elements = [...container.children];
 
-  container.addEventListener("mousemove", function (event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    let width = container.clientWidth;
-    let frame = Math.floor((x / width) * totalImages);
-    if (frame > totalImages - 1) frame = totalImages - 1;
-    // console.log(frame);
+  //   container.addEventListener("mousemove", function (event) {
+  //     let x = event.clientX;
+  //     let y = event.clientY;
+  //     let width = container.clientWidth;
+  //     let frame = Math.floor((x / width) * totalImages);
+  //     if (frame > totalImages - 1) frame = totalImages - 1;
+  //     // console.log(frame);
 
+  //     for (let i = 0; i < totalImages; i++) {
+  //       if (i === frame) {
+  //         elements[i].classList.add("show");
+  //         elements[i].classList.remove("hide");
+  //       } else {
+  //         elements[i].classList.add("hide");
+  //         elements[i].classList.remove("show");
+  //       }
+  //     }
+  //   });
+
+  let lastKnownScrollPosition = 0;
+  let ticking = false;
+
+  function doSomething(scrollPos) {
+    let width = container.clientWidth;
+    let frame = Math.floor((scrollPos / width) * totalImages);
+
+    let seekFrame = frame > totalImages - 1 ? totalImages - 1 : frame;
+    // if (frame > totalImages - 1) frame = totalImages - 1;
+    console.log(scrollPos);
     for (let i = 0; i < totalImages; i++) {
-      if (i === frame) {
+      if (i === seekFrame) {
         elements[i].classList.add("show");
         elements[i].classList.remove("hide");
       } else {
@@ -31,7 +52,25 @@ window.onload = function () {
         elements[i].classList.remove("show");
       }
     }
+    if (scrollPos >= width) {
+      let pos = scrollPos - width;
+      console.log(pos);
+      container.style.top = -pos + "px";
+    } else {
+      container.style.top = "0px";
+    }
+  }
 
-    // console.log(elements);
+  document.addEventListener("scroll", (event) => {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        doSomething(lastKnownScrollPosition);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
   });
 };
